dffddb302dba39e1cbf1fd382eb92e6d8cc7372d
LashenDEV <lashenkari@gmail.com>
2023-04-02 10:50:39 +0530

    Section 4: Building the initial graphical interface of the HTTP client

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('service_id');
+            $table->string('grant_type');
+            $table->text('access_token');
+            $table->text('refresh_token');
+            $table->timestamp('token_expires_at');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
